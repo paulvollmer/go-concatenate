f155764bb87bb3457441243ebb5da545c6bc0c36
Paul Vollmer <paul.vollmer@protonmail.com>
2016-11-26 01:30:36 +0100

    create concatenate manager

--- concatenate_test.go
+++ concatenate_test.go
@@ -49,3 +49,13 @@ func Test_FilesToFile(t *testing.T) {
 		t.Error("FilesToFile not equal")
 	}
 }
+
+func Test_Manager(t *testing.T) {
+	m := NewManager()
+	m.Set("tmp_test2.txt", "fixture/a.txt", "fixture/b.txt")
+	err := m.ProcessAll()
+	if err != nil {
+		t.Error(err)
+	}
+	m.Process("tmp_test2.txt")
+}
