4a437a0d68ae2c913cbfa360a5a850f6c9ee931e
Paul Vollmer <paul.vollmer@protonmail.com>
2016-11-28 10:51:22 +0100

    Create Makefile, add tiny docs to source

--- concatenate_manager.go
+++ concatenate_manager.go
@@ -2,17 +2,22 @@ package concatenate
 
 import "errors"
 
+// Manager manage a map of sources to concatenate
 type Manager map[string][]string
 
+// NewManager return a new Manager element
 func NewManager() *Manager {
 	m := Manager{}
 	m = make(map[string][]string, 0)
 	return &m
 }
+
+// Set a name and its sources to the Manager
 func (m *Manager) Set(name string, src ...string) {
 	(*m)[name] = src
 }
 
+// Process a given set
 func (m *Manager) Process(name string) error {
 	d, ok := (*m)[name]
 	if !ok {
@@ -22,6 +27,7 @@ func (m *Manager) Process(name string) error {
 	return err
 }
 
+// ProcessAll run the Process func at all sets
 func (m *Manager) ProcessAll() error {
 	for k, v := range *m {
 		err := FilesToFile(k, "\n", v...)
