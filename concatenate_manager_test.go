65cf02b7ff9423f4c6a4e99c8d6701308c5a9626
Paul Vollmer <paul.vollmer@protonmail.com>
2016-11-28 11:24:19 +0100

    FilesToBytes now support matching pattern

--- concatenate_manager_test.go
+++ concatenate_manager_test.go
@@ -4,12 +4,24 @@ import (
 	"testing"
 )
 
-func Test_Manager(t *testing.T) {
+var inputFiles = []string{"fixture/a.txt", "fixture/b.txt"}
+
+func Test_Manager_Process(t *testing.T) {
 	m := NewManager()
-	m.Set("tmp_test2.txt", "fixture/a.txt", "fixture/b.txt")
+	m.Set("tmp_test1.txt", inputFiles...)
+
+	err := m.Process("tmp_test1.txt")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Manager_ProcessAll(t *testing.T) {
+	m := NewManager()
+	m.Set("tmp_test2.txt", inputFiles...)
+
 	err := m.ProcessAll()
 	if err != nil {
 		t.Error(err)
 	}
-	m.Process("tmp_test2.txt")
 }
