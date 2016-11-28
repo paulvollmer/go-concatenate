57d1ae7b71dfb3f5a5a2bc49b4d93dbe937e2eca
Paul Vollmer <paul.vollmer@protonmail.com>
2016-11-28 10:39:03 +0100

    cleanup test

--- concatenate_test.go
+++ concatenate_test.go
@@ -6,14 +6,15 @@ import (
 )
 
 func Test_BytesToBytes(t *testing.T) {
-	// fmt.Println(string(BytesToBytes([]byte("-"), []byte("a1"), []byte("b1"))))
-	if string(BytesToBytes([]byte("-"), []byte("a1"), []byte("b1"))) != string([]byte("a1-b1")) {
+	result := BytesToBytes([]byte("-"), []byte("a1"), []byte("b1"))
+	if string(result) != string([]byte("a1-b1")) {
 		t.Error("BytesToBytes return not equal, must be 'a1-b1'")
 	}
 }
 
 func Test_StringsToString(t *testing.T) {
-	if StringsToString("-", "a", "b") != "a-b" {
+	result := StringsToString("-", "a", "b")
+	if result != "a-b" {
 		t.Error("StringsToString return not equal, must be 'a-b'")
 	}
 }
@@ -23,16 +24,21 @@ func Test_FilesToBytes(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	// fmt.Printf("S %q", src)
 	if string(src) != "a1\na2\n-b1\nb2\n" {
 		t.Error("FilesToBytes not equal")
+	}
+}
+
+func Test_FilesToBytes_NotFound(t *testing.T) {
+	_, err := FilesToBytes("-", "fixture/not_a.txt", "fixture/not_b.txt")
+	if err == nil {
+		t.Error("FilesToBytes missing error")
 	}
 }
 
 func Test_FilesToFile(t *testing.T) {
 	tmpTestFile := "tmp_test.txt"
 
-	// ConcatendateToFile("test.txt", "a", "b")
 	err := FilesToFile(tmpTestFile, "\n", "fixture/a.txt", "fixture/b.txt")
 	if err != nil {
 		t.Error(err)
@@ -43,7 +49,6 @@ func Test_FilesToFile(t *testing.T) {
 	if err != nil {
 		t.Error("FilesToFile written file does not exist")
 	}
-	// fmt.Println(string(d))
 	if string(d) != "a1\na2\n\nb1\nb2\n" {
 		t.Error("FilesToFile not equal")
 	}
