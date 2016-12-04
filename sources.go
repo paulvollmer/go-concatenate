ff9bc6d847dc5cba737f76a3ce903210188148b2
Paul Vollmer <paul.vollmer@protonmail.com>
2016-12-04 13:25:33 +0100

    sources add test cases

--- sources.go
+++ sources.go
@@ -1,5 +1,9 @@
 package concatenate
 
+import (
+	"path/filepath"
+)
+
 // Sources store a list of source paths
 type Sources []string
 
@@ -10,8 +14,11 @@ func NewSources() *Sources {
 	return &s
 }
 
-// Add a source to the list of sources, if not exist
+// Add a source to the Sources array. if src already exist, return false.
 func (s *Sources) Add(src string) bool {
+	if src == "" {
+		return false
+	}
 	for _, v := range *s {
 		if v == src {
 			return false
@@ -19,6 +26,26 @@ func (s *Sources) Add(src string) bool {
 	}
 	*s = append(*s, src)
 	return true
+}
+
+// GetFilepaths return a list of filepaths for the given source.
+// if the source is a glob, the function return all matched paths.
+func (s Sources) GetFilepaths(i int) ([]string, error) {
+	return filepath.Glob(s[i])
+}
+
+// GetAllFilepaths return a list of all filepaths
+func (s *Sources) GetAllFilepaths() ([]string, error) {
+	var paths []string
+	for i := range *s {
+		// is the path a glob?
+		glob, err := s.GetFilepaths(i)
+		if err != nil {
+			return paths, err
+		}
+		paths = append(paths, glob...)
+	}
+	return paths, nil
 }
 
 // Total return the total number of sources
--- sources_test.go
+++ sources_test.go
@@ -1,29 +1,119 @@
 package concatenate
 
 import (
+	"fmt"
 	"testing"
 )
 
-func Test_Sources(t *testing.T) {
+func Test_NewSources(t *testing.T) {
 	m := NewSources()
 	if m == nil {
 		t.Error("NewSources not type of Sources")
 	}
+	if len(*m) != 0 {
+		t.Error("NewSources not equal")
+	}
+}
 
-	if m.Total() != 0 {
-		t.Error("Sources Total not equal")
+func Test_Sources_Add(t *testing.T) {
+	testCases := []struct {
+		src           []string
+		expectedOK    bool
+		expectedTotal int
+	}{
+		{
+			src:           []string{""},
+			expectedOK:    false,
+			expectedTotal: 0,
+		},
+		{
+			src:           []string{"src-1"},
+			expectedOK:    true,
+			expectedTotal: 1,
+		},
+		{
+			src:           []string{"fixture/a.txt", "fixture/b.txt"},
+			expectedOK:    true,
+			expectedTotal: 2,
+		},
 	}
 
-	addOk := m.Add("a.txt")
-	if addOk == false {
-		t.Error("Add failed")
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%q", tc.src), func(t *testing.T) {
+
+			m := NewSources()
+
+			// test to Add() sources and check if the result is equal
+			for addI, addV := range tc.src {
+				ok := m.Add(addV)
+				if ok != tc.expectedOK {
+					t.Errorf("Add %v failed", addI)
+				}
+				// if the total expected size is higher than zero, we can check the array items
+				if tc.expectedTotal > 0 {
+					if (*m)[addI] != addV {
+						t.Errorf("Add source 0 not equal, must be %v", addV)
+					}
+
+					// // att the same source a second time
+					// addOk = m.Add("a.txt")
+					// if addOk == true {
+					// 	t.Error("Add must be fasle")
+					// }
+				}
+			}
+
+			// final check the expected total size of the array
+			if len(*m) != tc.expectedTotal {
+				t.Errorf("Add total sources not equal, must be %v", tc.expectedTotal)
+			}
+
+		})
 	}
-	if (*m)[0] != "a.txt" {
-		t.Error("Add not set to string array")
+}
+
+func Test_Sources_GetAllFilepaths(t *testing.T) {
+	testCases := []struct {
+		src      []string
+		expected []string
+	}{
+		{
+			[]string{"fixture/a.txt"},
+			[]string{"fixture/a.txt"},
+		},
+		{
+			[]string{"fixture/c/*.txt"},
+			[]string{"fixture/c/c1.txt", "fixture/c/c2.txt", "fixture/c/c3.txt"},
+		},
+		{
+			[]string{"fixture/d/**/*"},
+			[]string{"fixture/d/d1/d1.json", "fixture/d/d1/d1.txt", "fixture/d/d2/d2.json", "fixture/d/d2/d2.txt"},
+		},
+		{
+			[]string{"fixture/d/**/*.json"},
+			[]string{"fixture/d/d1/d1.json", "fixture/d/d2/d2.json"},
+		},
 	}
-	// att the same source a second time
-	addOk = m.Add("a.txt")
-	if addOk == true {
-		t.Error("Add must be fasle")
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%q", tc.src), func(t *testing.T) {
+
+			m := Sources{}
+			m = make([]string, len(tc.src))
+			m = tc.src
+
+			f, err := m.GetAllFilepaths()
+			if err != nil {
+				t.Error(err)
+			}
+			if len(f) != len(tc.expected) {
+				t.Errorf("GetAllFilepaths lenght not equal, must be %v", len(tc.expected))
+			}
+			for i := range f {
+				if f[i] != tc.expected[i] {
+					t.Errorf("GetFilepaths not equal at %v", i)
+				}
+			}
+		})
 	}
 }
