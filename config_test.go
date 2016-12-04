ecbcc2bc9da48bc265b3397e2ad253a64bfcfef1
Paul Vollmer <paul.vollmer@protonmail.com>
2016-12-04 23:47:48 +0100

    create config

---
+++ config_test.go
@@ -0,0 +1,63 @@
+package concatenate
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_NewConfig(t *testing.T) {
+	cfg := NewConfig()
+	if cfg == nil {
+		t.Error("Config not type of Config")
+	}
+}
+
+func Test_Config(t *testing.T) {
+	testCases := []struct {
+		data       map[string][]string
+		totalFiles int
+	}{
+		{
+			data: map[string][]string{
+				"test-1": []string{"fixture/a.txt"},
+			},
+			totalFiles: 1,
+		},
+		{
+			data: map[string][]string{
+				"test-1": []string{"fixture/a.txt", "fixture/b.txt"},
+			},
+			totalFiles: 2,
+		},
+		// {
+		// 	name:       "test-3",
+		// 	sources:    []string{"fixture/c/*"},
+		// 	totalFiles: 3,
+		// },
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			cfg := NewConfig()
+			for name, src := range tc.data {
+				cfg.Add(name, src...)
+			}
+
+			if cfg.TotalSets() != 1 {
+				t.Error("TotalSets not equal")
+			}
+
+			totalFiles, err := cfg.TotalFiles()
+			if err != nil {
+				t.Error(err)
+			}
+			if totalFiles != tc.totalFiles {
+				t.Error("TotalFiles not equal")
+			}
+
+			// if cfg.TotalFilesInSet(tc.name) != tc.totalFiles {
+			// 	t.Error("TotalFilesInSet not equal")
+			// }
+		})
+	}
+}
