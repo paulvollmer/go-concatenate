480625a69e9b5e718c0e7f01f5d70c50663a046b
Paul Vollmer <paul.vollmer@protonmail.com>
2016-12-05 07:04:41 +0100

    refactor, init cli

---
+++ bin/concat/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/paulvollmer/go-concatenate"
+)
+
+var version = "0.1.0"
+
+func usage() {
+	fmt.Println("Usage: fastconcat out [in]")
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+}
+
+func main() {
+	flagVersion := flag.Bool("v", false, "print the version and exit")
+	flagConfig := flag.String("c", "", "read a json config")
+	flag.Usage = usage
+	flag.Parse()
+	if *flagVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+	if *flagConfig != "" {
+		m := concatenate.NewManager()
+		err := m.ReadConfig(*flagConfig)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(127)
+		}
+		fmt.Println("config", m.Config)
+		os.Exit(0)
+	}
+
+	totalArgs := len(os.Args)
+
+	if totalArgs == 1 {
+		usage()
+		os.Exit(127)
+	}
+	if totalArgs == 2 {
+		fmt.Println("Missing input sources")
+		os.Exit(127)
+	}
+	if totalArgs > 2 {
+		out := os.Args[1]
+		sources := os.Args[2:]
+
+		fmt.Println("out", out)
+		fmt.Println("sources", sources)
+
+	}
+}
--- concatenate_manager.go
+++ concatenate_manager.go
@@ -1,68 +1,38 @@
 package concatenate
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"os"
-	"path/filepath"
 )
 
-// Manager manage a map of sources to concatenate
-// TODO: chacke file body to faster concatenate files
-type Manager map[string]Sources
+// TODO: cache file content to faster concatenate a set
+type Manager struct {
+	Config Config
+	Cache  string
+}
 
 // NewManager return a new Manager element
 func NewManager() *Manager {
 	m := Manager{}
-	m = make(map[string]Sources, 0)
+	m.Config = *NewConfig()
+	// m = make(map[string]Sources, 0)
 	return &m
 }
 
-// // AddSource add a list of sources to a specific set
-// func (s *Sources) Add(src ...string) bool {
-// 	_, ok := (*m)[name]
-// 	if ok {
-// 		return false
-// 	}
-// 	(*m)[name] = append((*m)[name], src...)
-// 	return true
-// }
-
-// Add a name and its sources to the Manager
-func (m *Manager) Add(name string, src ...string) bool {
-	_, ok := (*m)[name]
-	if ok {
-		return false
+func (m *Manager) ReadConfig(filename string) error {
+	d, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
 	}
-	(*m)[name] = src
-	return true
-}
-
-// TotalSets return the number of total sets
-func (m *Manager) TotalSets() int {
-	return len((*m))
-}
-
-// TotalFiles return the number of files
-func (m *Manager) TotalFiles() int {
-	counter := 0
-	for _, v := range *m {
-		counter += len(v)
-	}
-	return counter
-}
-
-// TotalFilesInSet return the number of files of a sepcific set
-func (m *Manager) TotalFilesInSet(name string) int {
-	d, ok := (*m)[name]
-	if !ok {
-		return 0
-	}
-	return len(d)
+	err = json.Unmarshal(d, &m.Config)
+	return err
 }
 
 // Process a given set
 func (m *Manager) Process(filename string, perm os.FileMode) error {
-	d, ok := (*m)[filename]
+	d, ok := m.Config[filename]
 	if !ok {
 		return errors.New(filename + " not found")
 	}
@@ -72,66 +42,11 @@ func (m *Manager) Process(filename string, perm os.FileMode) error {
 
 // ProcessAll run the Process func at all sets
 func (m *Manager) ProcessAll(perm os.FileMode) error {
-	for k, v := range *m {
+	for k, v := range m.Config {
 		err := FilesToFile(k, perm, "\n", v...)
 		if err != nil {
 			return err
 		}
 	}
 	return nil
-}
-
-// ExistSource return true i the given source was found at the sets
-func (m *Manager) ExistSource(src string) bool {
-	for _, v := range *m {
-		for _, v2 := range v {
-			matches, _ := filepath.Glob(v2)
-			for _, item := range matches {
-				if item == src {
-					return true
-				}
-			}
-		}
-	}
-	return false
-}
-
-// GetDirs return a list of all target directories
-func (m *Manager) GetDirs() []string {
-	dirs := make([]string, 0)
-	tmpIndex := make(map[string]int, 0)
-	for k := range *m {
-		kdir := filepath.Dir(k)
-		_, ok := tmpIndex[kdir]
-		// fmt.Println(k, kdir, ok)
-		if !ok {
-			dirs = append(dirs, kdir)
-			tmpIndex[kdir] = 1
-		} else {
-			tmpIndex[kdir]++
-		}
-	}
-	return dirs
-}
-
-// GetDirsOfSources return a list of all source directories
-func (m *Manager) GetDirsOfSources() []string {
-	dirs := make([]string, 0)
-	tmpIndex := make(map[string]int, 0)
-	for _, target := range *m {
-		// fmt.Println(target)
-		for _, fname := range target {
-			kdir := filepath.Dir(fname)
-			_, ok := tmpIndex[kdir]
-			// fmt.Println(kdir, ok)
-			if !ok {
-				dirs = append(dirs, kdir)
-				tmpIndex[kdir] = 1
-			} else {
-				tmpIndex[kdir]++
-			}
-		}
-
-	}
-	return dirs
 }
